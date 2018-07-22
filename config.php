201ff5ce75c29636382f7aad2b65c679d4ba647d
Konthorn <konthorn.roj@gmail.com>
2018-07-22 17:31:16 +0700

    fix(config.php)

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'teams' => [
+        [
+            'name' => 'ขาว',
+            'groupID' => "Cd378bc878914d95778542d8fc9ef5b87"
+        ],
+        [
+            'name' => 'แดง',
+            'groupID' => "id1"
+        ],
+        [
+            'name' => 'ฟ้า',
+            'groupID' => "id2"
+        ],
+    ],
+];
