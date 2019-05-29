cb5aa0fdd4cb425db3a0737ecaa559ca6f1853d4
konthornR <konthorn.roj@gmail.com>
2019-05-29 17:16:35 +0700

    chore: change team color

--- config.php
+++ config.php
@@ -3,15 +3,15 @@
 return [
     'teams' => [
         [
-            'name' => 'ขาว',
+            'name' => 'ฟ้า',
             'groupID' => 'C1be16be9e677112d95877d1e00f2451f',
         ],
         [
-            'name' => 'แดง',
+            'name' => 'เหลือง',
             'groupID' => 'C1deedb180814dacfd4f2bc5c3d3b4fba',
         ],
         [
-            'name' => 'ฟ้า',
+            'name' => 'ขาว',
             'groupID' => 'C65a5717c0494178d5f2aa984b4ea69b2',
         ],
     ],
